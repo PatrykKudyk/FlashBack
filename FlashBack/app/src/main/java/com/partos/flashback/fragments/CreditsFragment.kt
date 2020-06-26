1df05288d6afd6d83c2239cb04cc2b33ddf5011f
Papryk <gustaw5022@gmail.com>
2020-06-27 00:05:36 +0200

    added layout

--- FlashBack/app/src/main/java/com/partos/flashback/fragments/CreditsFragment.kt
+++ FlashBack/app/src/main/java/com/partos/flashback/fragments/CreditsFragment.kt
@@ -29,17 +29,13 @@ private const val ARG_PARAM2 = "param2"
  * Use the [AccountFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainMenuFragment : Fragment() {
+class CreditsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
 
     private lateinit var rootView: View
-    private lateinit var image: ImageView
-    private lateinit var linearLayout: LinearLayout
-    private lateinit var loginButton: Button
-    private lateinit var creditsButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,7 +50,7 @@ class MainMenuFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        rootView = inflater.inflate(R.layout.fragment_main_menu, container, false);
+        rootView = inflater.inflate(R.layout.fragment_credits, container, false);
         initFragment()
         return rootView
     }
@@ -85,42 +81,13 @@ class MainMenuFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() =
-            MainMenuFragment().apply {
+            CreditsFragment().apply {
                 arguments = Bundle().apply {
                 }
             }
     }
 
     private fun initFragment() {
-        image = rootView.findViewById(R.id.menu_image_view)
-        linearLayout = rootView.findViewById(R.id.main_linear_layout)
-        loginButton = rootView.findViewById(R.id.menu_button_log_in)
-        creditsButton = rootView.findViewById(R.id.menu_button_credits)
 
-        image.setOnClickListener {
-            val animation = AnimationUtils.loadAnimation(rootView.context, R.anim.disapear_front)
-            image.startAnimation(animation)
-            Handler().postDelayed({
-                image.visibility = View.GONE
-                linearLayout.visibility = View.VISIBLE
-                val anim1 = AnimationUtils.loadAnimation(rootView.context, R.anim.enter_left_to_right)
-                val anim2 = AnimationUtils.loadAnimation(rootView.context, R.anim.enter_right_to_left)
-                loginButton.startAnimation(anim1)
-                creditsButton.startAnimation(anim2)
-            }, 500)
-        }
-
-        creditsButton.setOnClickListener {
-            val creditsFragment = CreditsFragment.newInstance()
-            fragmentManager
-                ?.beginTransaction()
-                ?.setCustomAnimations(
-                    R.anim.enter_bottom_to_top, R.anim.exit_top_to_bottom,
-                    R.anim.enter_top_to_bottom, R.anim.exit_bottom_to_top
-                )
-                ?.replace(R.id.main_frame_layout, creditsFragment)
-                ?.addToBackStack(CreditsFragment.toString())
-                ?.commit()
-        }
     }
 }
