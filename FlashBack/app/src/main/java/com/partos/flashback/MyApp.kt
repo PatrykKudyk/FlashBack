1a3fddde6210914437cb0aa051d450855b55f866
Papryk <gustaw5022@gmail.com>
2020-07-12 21:44:34 +0200

    added database

--- FlashBack/app/src/main/java/com/partos/flashback/MyApp.kt
+++ FlashBack/app/src/main/java/com/partos/flashback/MyApp.kt
@@ -1,10 +1,12 @@
 package com.partos.flashback
 
 import android.app.Application
+import com.partos.flashback.models.Settings
 
 class MyApp: Application() {
     companion object {
         var userId = (-1).toLong()
+        var userSettings = Settings(-1,-1, 15, 5, 10)
     }
 }
 
